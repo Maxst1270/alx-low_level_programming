1271eab15eb29b7eb304c2b2da481cc975a3e8f1
Maxst1270 <alisuna225@gmail.com>
2023-09-07 09:39:31 -0700

    final fx

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,12 +2,14 @@
 
 /**
  * main - is an Entry point
+ *
  * Description: it is printing program
- * return: always return value 0
+ *
+ * Return: always return value 0 (success)
  */
 
 int main(void)
 {
-	 puts("\"Programming is like building a multilingual puzzle");
-	 return(0);
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
