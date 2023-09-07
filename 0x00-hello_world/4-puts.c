7c3aebef9cea50cde59d2352fd97faf8d6fdf5fc
Maxst1270 <alisuna225@gmail.com>
2023-09-07 09:23:20 -0700

    qck fix

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,14 @@
 #include<stdio.h>
-int main(void)
+
+/**
+ * main- is an Entry point
+ * 
+ * dis: it is printing program
+ * 
+ * returns: a constant return value  0
+ * /
+
+ int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	return(0);
