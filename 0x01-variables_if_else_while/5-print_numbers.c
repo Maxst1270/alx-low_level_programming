8983cdce1396a3eef94171efd133a20eb775dc81
Maxst1270 <alisuna225@gmail.com>
2023-09-08 14:40:51 -0700

    fifth code

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ *main - Entry point
+ *
+ * Description:prints numbers less than ten
+ *
+ * Return:always 0
+*/
+int main(void)
+{
+	int c = 0;
+
+	while (c < 10)
+	{
+		printf(c);
+		c++;
+	}
+
+	printf('\n');
+
+	return (0);
+}
