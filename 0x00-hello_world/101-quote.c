04a57a9aab8b6791dc9716844d0891262ddce6ae
Maxst1270 <alisuna225@gmail.com>
2023-09-10 05:31:13 -0700

    101 -recap

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,19 @@
+#include <unistd.h>
+
+/**
+ * main - Entry point
+ *
+ * Description:printing a one liner
+ *
+ * Return:returns 1 unsuccessful
+*/
+
+int main(void)
+{
+	char pnt[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+
+	write(1, pnt, 59);
+
+	return (1);
+}
+
