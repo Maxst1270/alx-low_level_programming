e39dc76d24ed9fc46842b070cd37d437614d4aea
Maxst1270 <alisuna225@gmail.com>
2023-09-12 12:12:46 -0700

    third strike

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - a function that the prints da alphabet x10
+ *
+ */
+
+void print_alphabet_x10(void)
+{
+	int i = 0;
+
+	while (i < 10)
+	{
+		char lp = 'a';
+
+		while (lp < = 'z')
+		{
+			_putchar(lp);
+			lp++;
+		}
+
+	_putchar('\n');
+	i++;
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -6,5 +6,7 @@ int _putchar(char k);
 
 void print_alphabet(void);
 
+void print_alphabet_x10(void);
+
 
 #endif
