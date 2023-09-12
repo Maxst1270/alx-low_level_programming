554db53a50ec6db5c0dc6421c4fb8dcdc35815ba
Maxst1270 <alisuna225@gmail.com>
2023-09-12 11:30:16 -0700

    strike two

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main"
+
+/**
+ * print_alphabet - a function that prints the alphabet in lower cas
+ */
+
+void print_alphabet(void)
+{
+	char lp = 'a';
+
+	while (lp <= 'z')
+	_putchar(lp);
+	lp++;
+
+	_putchar('\n');
+}
