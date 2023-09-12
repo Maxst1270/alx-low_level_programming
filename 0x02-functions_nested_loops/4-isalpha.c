e355ceb9dca90b875bf06a971efc80c4e43f4aff
Maxst1270 <alisuna225@gmail.com>
2023-09-12 14:37:00 -0700

    5- is alpha

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _isalpha - checks for lowercase or uppercase letters
+ *
+ * @c: checks the input
+ *
+ * Return:returns1 if it is aletter ,0 if otherwise
+ */
+
+int _isalpha(int c)
+{
+	if (c >= 65 && c <= 90 || c >= 97 && c <= 122)
+		return (1);
+
+	return (0);
+}
