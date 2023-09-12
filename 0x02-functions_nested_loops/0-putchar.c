6e00616493802cca283f638f44ecda523d4a0952
Maxst1270 <alisuna225@gmail.com>
2023-09-12 09:27:18 -0700

    first code addition

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,23 @@
+#include "mmain.h"
+
+/**
+ * main - Entry point
+ *
+ * Description: print the string _putchar using prototype
+ *
+ * Return: returns 0
+ */
+
+int main(void)
+{
+	char srn[] = "_putchar";
+	int cn = 0;
+
+	while (cn < 8)
+	{
+	_putchar(sr[cn]);
+	cn++;
+	}
+	_putchar('\n');
+	return (0);
+}
