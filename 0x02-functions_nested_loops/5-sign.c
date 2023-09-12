65b26e14141715174d72fed65eec94ef1697fe1d
Maxst1270 <alisuna225@gmail.com>
2023-09-12 15:08:18 -0700

    6 sign fx

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -11,15 +11,19 @@
 int print_sign(int n)
 {
 	if (n > 0)
+	{
 		_putchar('+');
 	return (1);
+	}
 
 	else if (n < 0)
+	{
 		_putchar('-');
 	return (-1);
+	}
 
 	else
-		_putchar(0 + '48');
+		_putchar(48);
 
 	return (0);
 
