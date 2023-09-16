348bebb19f37202c6a40ae237b9b518b4ce260f8
Maxst1270 <alisuna225@gmail.com>
2023-09-15 21:16:58 -0700

    and we are back

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+
+ /**
+  * add - function that adds two integers
+  *
+  *@v:first integer input
+  *@w:second integer input
+  *
+  * Return:sum of v and w
+ */
+
+int add(int v, int w)
+{
+	int sum = v + w;
+
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+/**
+ * times_table - prints the times table 9
+*/
+
+void times_table(void)
+{
+	int j, v, p;
+
+	for (j = 0; j <= 9; j++)
+	{
+		_putchar(48);
+
+		for (v = 1; v <= 9; v++)
+		{
+			_putchar(',');
+			_putchar(' ');
+
+			p = j * v;
+
+			if (p <= 9)
+				 _putchar(' ');
+
+			else
+				 _putchar((p / 10) + 48);
+			 _putchar((p % 10) + 48);
+		}
+		_puthar('\n');
+	}
+
+}
