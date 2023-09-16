3d2b1f994e74fecccbec0c03c4f7d0239c3c6b03
Maxst1270 <alisuna225@gmail.com>
2023-09-16 11:56:24 -0700

    counter

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ *print_to_98 - counts from n to 98
+ *
+ *@n:beginning int
+*/
+
+void print_to_98(int n)
+{
+	int par;
+
+	if (n > 98)
+		while (n > par)
+		{
+			printf("%d", par);
+			n--;
+		}
+	else
+		while (n < 98) 
+		{
+			printf("%d ", par);
+		n++;
+		}
+
+}
