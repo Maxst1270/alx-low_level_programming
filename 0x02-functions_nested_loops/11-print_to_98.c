a350dad9419a13650b25eb9640dcb6a340f24938
Maxst1270 <alisuna225@gmail.com>
2023-09-16 22:04:22 -0700

    trial no 5

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -13,11 +13,11 @@ void print_to_98(int n)
 
 	if (n > 98)
 		for (par = n; par > 98; par--)
-			printf("%d", par);
+			printf(", %d", par);
 
 	else
 		for (par = n; par < 98; par++)
-			printf("%d ", par);
+			printf(", %d", par);
 	printf("98\n");
 
 }
