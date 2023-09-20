a7e210299ba6949a86a76752f8151ead8a525008
Maxst1270 <alisuna225@gmail.com>
2023-09-20 05:18:20 -0700

    time to print

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_numbers - prints 10 number
+*/
+
+void print_numbers(void)
+{
+	int k = 48;
+
+	while (k <= 57)
+	{
+		_putchar('%d', k);
+	}
+	_putchar('\n');
+}
