1459536e4066d805bdf85c55603563882fd349c8
Maxst1270 <alisuna225@gmail.com>
2023-09-20 04:46:25 -0700

    mult function

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,19 @@
+#inclide "main.h"
+
+/**
+ * mul - multiplies two integers
+ *
+ * @a: first integer to mult
+ * @b: second integer to mult
+ *
+ *Return:returns fin
+*/
+
+int mul(int a, int b)
+{
+	int fin;
+
+	fin = a * b;
+
+	return (fin);
+}
