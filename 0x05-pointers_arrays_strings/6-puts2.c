d19b42d280a962357a4b72f578c1700756f2c788
Maxst1270 <alisuna225@gmail.com>
2023-09-21 15:16:43 -0700

    another puts

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * puts2 - prints everyother char
+ *
+ * @str:parameter for string
+ *
+ * Return:no return
+*/
+
+void puts2(char *str)
+{
+	int x;
+
+	for (x = 0; str[x] != '\0'; x += 2)
+	{
+		_putchar(str[x]);
+	}
+	_putchar('\n');
+}
