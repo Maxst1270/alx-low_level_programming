a0e5fb2429ed33eef21d921e5875fb4d39103cd8
Maxst1270 <alisuna225@gmail.com>
2023-09-21 08:15:38 -0700

    pointers 1

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ *reset_to_98 - swaps values of pointed parameter
+ *
+ * @n: pointer as parameter used
+ *
+ * Return:no return
+*/
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -3,5 +3,6 @@
 
 #include <stdio.h>
 int _putchar(char k);
+void reset_to_98(int *n);
 
 #endif
