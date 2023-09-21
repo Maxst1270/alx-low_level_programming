83b1de423a16a8630f882688acdce9506e7191bf
Maxst1270 <alisuna225@gmail.com>
2023-09-21 13:07:41 -0700

    pointer6

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_rev - prints the string reversed
+ *
+ * @s:string pointer
+*/
+
+void print_rev(char *s)
+{
+	int rv;
+
+	for (rv = 0; s[rv]; rv++)
+
+	while (rv--)
+		_putchar(s[rv]);
+	_putchar('\n');
+}
