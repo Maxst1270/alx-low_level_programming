128c25fcd3bcbd9fb4a419ce2573f95e8d37a36d
Maxst1270 <alisuna225@gmail.com>
2023-09-22 15:02:23 -0700

    last mand fx

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -11,12 +11,12 @@
 
 char *_strcpy(char *dest, char *src)
 {
-	int j;
+	int j = -1;
 
-	for (j = -1; src[j] != '\0'; j++)
-	{
+	do {
+		j++;
 		dest[j] = src[j];
-	}
+	} while (src[j] != '\0');
 
 	return (dest);
 }
