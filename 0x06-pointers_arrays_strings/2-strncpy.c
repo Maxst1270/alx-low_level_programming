f66fd071a09be81da7c951d10cb934be12d2a9a2
Maxst1270 <alisuna225@gmail.com>
2023-09-22 18:37:56 -0700

    strike 2

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -12,13 +12,10 @@
 
 char *_strncpy(char *dest, char *src, int n)
 {
-	int x = 0;
+	int x;
 
-	while (x < n && src[x] != '\0')
-	{	
+	for (x = 0; x < n && src[x] != '\0'; x++)
 		dest[x] = src[x];
-		x++;
-	}
 
 	for (; x < n; x++)
 		dest[x] = '\0';
