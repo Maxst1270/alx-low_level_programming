2422843cd28207e7616cc2248858e1300b745987
Maxst1270 <alisuna225@gmail.com>
2023-09-22 18:18:58 -0700

    boyaa

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * *_strncpy - copys a string
+ *
+ * @dest:copied string
+ * @src:string copied from
+ * @n:string par
+ *
+ * Return:returns copied string
+*/
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	int x = 0;
+
+	while (x < n && src[x] != '\0')
+		dest[x] = src[x];
+
+	for (; x < n; x++)
+		dest[x] = '\0';
+
+	return (dest);
+}
