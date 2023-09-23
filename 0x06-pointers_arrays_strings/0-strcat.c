306cb7105955948f496528e2b700c984f884eb71
Maxst1270 <alisuna225@gmail.com>
2023-09-22 17:43:23 -0700

    point  1

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * *_strcat - concatenates two strings
+ *
+ * @dest:first string
+ * @src:second string
+ *
+ * Return:returns dest
+*/
+
+char *_strcat(char *dest, char *src)
+{
+	int i, is = 0;
+
+	for (i = 0; dest[i; i++])
+
+		while (src[is])
+		{
+			dest[i++] = src[is;]
+			is++;
+		}
+
+	return (dest);
+}
