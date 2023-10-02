0098c6e1570001f9cffdf574151b17a14a00d09e
Maxst1270 <alisuna225@gmail.com>
2023-10-02 16:33:10 -0700

    opt

---
+++ 0x0A-argc_argv/100-change.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Entry point
+ *
+ * @argc:number of arguments
+ * @argv:array of arguments
+ *
+ * Return:returns 0
+*/
+
+int main(int argc, char *argv[])
+{
+	if (argc == 2)
+	{
+		int k, lescent = 0, cash = atoi(argv[1]);
+		int cent[] = {25, 10, 5, 2, 1};
+
+		for (k = 0; k < 5; k++)
+		{
+			if (cash >= cent[k])
+			{
+				lescent += cash / cent[k];
+				cash = cash % cent[k];
+				if (cash % cent[k] == 0)
+				{
+					break;
+				}
+			}
+		}
+		printd("%d\n", lescent);
+	}
+	else
+	{
+		printf("Error\n");
+		return (1);
+	}
+	return (0);
+}
