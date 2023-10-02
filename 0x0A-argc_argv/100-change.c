843d2b80a564b12df651e5e2536e6f6c25494d3e
Maxst1270 <alisuna225@gmail.com>
2023-10-02 16:45:16 -0700

    lets see

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -14,14 +14,14 @@ int main(int argc, char *argv[])
 {
 	if (argc == 2)
 	{
-		int k, lescent = 0, cash = atoi(argv[1]);
+		int k, leastcent = 0, cash = atoi(argv[1]);
 		int cents[] = {25, 10, 5, 2, 1};
 
 		for (k = 0; k < 5; k++)
 		{
 			if (cash >= cents[k])
 			{
-				lescent += cash / cents[k];
+				leastcent += cash / cents[k];
 				cash = cash % cents[k];
 				if (cash % cents[k] == 0)
 				{
@@ -29,7 +29,7 @@ int main(int argc, char *argv[])
 				}
 			}
 		}
-		printd("%d\n", lescent);
+		printd("%d\n", leastcent);
 	}
 	else
 	{
