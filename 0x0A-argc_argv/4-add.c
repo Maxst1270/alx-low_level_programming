560e5c7dcd45d7048d0754bc315e7d0b846dc5ec
Maxst1270 <alisuna225@gmail.com>
2023-10-02 15:04:02 -0700

    final mand

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ *main - Entry point
+ *
+ *@argc:number of arguments
+ *@argv:array of arguments
+ *
+ *Return:retutns 0
+*/
+
+int main(int argc, char *argv[])
+{
+	int col;
+	char *k;
+
+	while (argc--)
+	{
+		for (k = argv[argc]; *k; k++)
+			if (*k < '0' || *k > '9')
+				return (printf("error\n"), 1);
+		col += atoi(argv[argc]);
+	}
+	printf("%d\n", col);
+	return (0);
+}
