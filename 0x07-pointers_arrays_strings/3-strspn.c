d33c8a1399ad38925ac03e278c4af40ba6a9a04d
Maxst1270 <alisuna225@gmail.com>
2023-10-02 19:50:38 -0700

    strike 4

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,28 @@
+#include "man.h"
+
+/**
+ * _strspn - gets length of a prefix substring
+ *
+ * @s:a string
+ * @accept:bytes of string
+ *
+ * Return:length
+*/
+
+unsigned int _strspn(char *s, char *accept)
+{
+	unsigned int str;
+
+	for (str = 0; s[str] != '\0'; str++)
+	{
+		unsigned int jpr = 0
+
+		while (accept[jpr] != s[str])
+		{
+			if (accept[jpr] == '\0')
+				return (str);
+			jpr++;
+		}
+	}
+	return (str);
+}
