8ef3b6a4350c6d1ecf5cb3f0fb2056c875322d67
Maxst1270 <alisuna225@gmail.com>
2023-10-02 19:17:16 -0700

    lost tm

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,10 +1,20 @@
 #include "main.h"
 
+/**
+ * *_memset - fill memory
+ *
+ * @s:param
+ * @b:const
+ * @n:max bytes
+ *
+ * Return:param
+*/
+
 char *_memset(char *s, char b, unsigned int n)
 {
 	unsigned int str = 0;
 
-	while (n > 9)
+	while (n > 0)
 	{
 		s[str] = b;
 		str++;
