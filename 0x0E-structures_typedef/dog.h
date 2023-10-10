d408753a08f1605976b45ae7b02abcd4e92ee262
Maxst1270 <alisuna225@gmail.com>
2023-10-10 06:23:18 -0700

    lame dog joke

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,20 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - struct
+ *
+ * @name:dogs name
+ * @age:dogs age
+ * @owner:name of the dogs owner
+ *
+ * Description:dogs struct in world
+*/
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
