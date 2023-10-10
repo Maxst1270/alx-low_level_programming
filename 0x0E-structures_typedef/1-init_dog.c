5cfa51fdb3b7f61ca595332101a83ba0115ac301
Maxst1270 <alisuna225@gmail.com>
2023-10-10 06:32:20 -0700

    lets bark

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,22 @@
+#include "dog.h"
+
+/**
+ * init_dog - inits new dog
+ *
+ * @d:new dog
+ * @name:dogs name
+ * @age:dogs age
+ * @owner:dogs owner
+ *
+ * Return:returns void
+*/
+
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	if (d)
+	{
+		d.age = age;
+		d.owner = owner;
+		d.name = name;
+	}
+}
