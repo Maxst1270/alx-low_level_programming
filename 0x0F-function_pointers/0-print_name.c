366abac98ecadf7b5e730405ae23464664d35721
Maxst1270 <alisuna225@gmail.com>
2023-10-11 08:14:23 -0700

    hmm

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,16 @@
+#include "function_pointers.h"
+#include <stdlib.h>
+
+/**
+ * print_name - prints a name
+ *
+ * @name:pointer to name
+ * @f:function pointer
+ *
+ * Return:returns void
+*/
+
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+}
