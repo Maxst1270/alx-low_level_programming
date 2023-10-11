00a4567f87e4adeb1bec5c4058c884f940191377
Maxst1270 <alisuna225@gmail.com>
2023-10-11 11:26:56 -0700

    lol

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,31 @@
+#include "function_pointers.h"
+
+/**
+ * int_index - searches for an int
+ *
+ * @array:point to array
+ * @size:size of the given array
+ * @cmp:point to a function that compares int
+ *
+ * Return:returns index
+ */
+
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int v;
+
+	if (size <= 0)
+		return (-1);
+
+	if (array && size && cmp)
+	{
+		for (v = 0; v < size; v++)
+		{
+			cmp(array[v]);
+			return (v);
+		}
+	}
+
+	return (-1);
+
+}
