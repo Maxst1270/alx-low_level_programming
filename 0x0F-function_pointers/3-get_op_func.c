6adde754392bf26c3c9d384b85c143861cef0f80
Maxst1270 <alisuna225@gmail.com>
2023-10-11 18:12:44 -0700

    look

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -18,9 +18,9 @@ int (*get_op_func(char *s))(int, int)
 		{"%", op_mod},
 		{NULL, NULL}
 	};
-	int i = 1;
+	int i = 0;
 
-	while (i <= 5)
+	while (i < 5)
 	{
 		if (ops[i].op[0] == *s && strlen(s) == 1)
 			return (ops[i].f);
