85b4df06858e916f55007c57d7029a4fc35d9496
Maxst1270 <alisuna225@gmail.com>
2023-10-25 13:14:32 -0700

    lists 3

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,34 @@
+#include "lists.h"
+
+/**
+ * add_node - add a node to the lists beginning
+ *
+ * @head:the pointer to nodes head address
+ * @str:node field
+ *
+ * Return:list size
+*/
+
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t *w_head = malloc(sizeof(list_t));
+
+	if(!w_head || !head)
+		return (NULL);
+
+	if (str)
+	{
+		w_head->str = strdup(str);
+		if (!w_head->str)
+		{
+			free(w_head);
+			return (NULL);
+		}
+		w_head->len = _strlen(w_head->str);
+	}
+
+	w_head->next = *head;
+	*head = w_head;
+
+	return (w_head);
+}
