dd2dbb72c5bc5f7ca2ad720ba91674d7b606ac56
Maxst1270 <alisuna225@gmail.com>
2023-11-16 11:16:55 +0200

    recursion proj

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * _sqrt_recursion -calcs the natural sqrt of number
+ *
+ * @n:numb given
+ *
+ * Return: sqrt ,or -1 otherwise
+*/
+int sqr(int n, int sq);
+int _sqrt_recursion(int n)
+{
+	return (sqr(n, 1));
+}
+
+/**
+ * sqr - checks for squares of numb
+ *
+ * @n:number given
+ * #sq:nat square root
+ *
+ * Return:returns -1,square root otherwise
+*/
+
+int sqr(int n, int sq)
+{
+	if (n == (sq * sq))
+		return (sq);
+	else if (n > (sq * sq))
+		return (sqr(n, sq + 1));
+	else
+		return (-1);
+}
---
+++ 0x08-recursion/README.md
@@ -0,0 +1,1 @@
+the Recursion project once more
