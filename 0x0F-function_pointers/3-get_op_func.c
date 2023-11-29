a854b2bc1faa744f9eb1c5a2a83513dee59b16bd
Maxst1270 <alisuna225@gmail.com>
2023-11-29 19:02:43 +0200

    function pointers

---
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -0,0 +1,33 @@
+#include "3-calc.h"
+
+/**
+ * get_op_func - finds the matching op
+ *
+ * @s:pointer
+ *
+ * Return:returns int
+*/
+
+int (*get_op_func(char *s))(int, int)
+{
+	op_t ops[] = {
+		{"+", op_add},
+		{"-", op_sub},
+		{"*", op_mul},
+		{"/", op_div},
+		{"%", op_mod},
+		{NULL, NULL}
+	};
+	int i = 0;
+
+	while (i < 5)
+	{
+		if (s && s[0] == ops[i].op[0])
+		{
+			return (ops[i].f);
+		}
+		i++;
+	}
+	return (NULL);
+
+}
